67dbd8df74f4eb3fbebf3ada833bc4f845d5349a
Vyacheslav <46050023+honeydrums@users.noreply.github.com>
2020-07-31 17:48:48 +0300

    fixes fonts, realize faq page

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// var passwordEyes = document.querySelectorAll('.password-control');
+
+// for (var passwordEye of passwordEyes) {
+//     this.addEventListener('click', function() {
+//             passwordEye.classList.toggle('view');
+//     })
+// }
